e5a71f5a50af51600ec140abc33119305af1c524
Han Chen <hc0402@berkeley.edu>
2015-10-12 23:01:06 -0700

    proj1 submit

---
+++ To-Do List/To-Do List/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  To-Do List
+//
+//  Created by Han Chen on 10/11/15.
+//  Copyright © 2015 Han Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddView : UIViewController {
+    
+    @IBOutlet var TextField: UITextField!
+    override func viewDidLoad() {
+        
+        
+    }
+}
+
---
+++ To-Do List/To-Do List/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  To-Do List
+//
+//  Created by Han Chen on 10/11/15.
+//  Copyright © 2015 Han Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+//    @IBAction func AddButton(sender: AnyObject) {
+//        let storyBoard: UIStoryboard = UIStoryboard(name: "main", bundle: nil)
+//        let vc: UINavigationController = storyBoard.instantiateViewControllerWithIdentifier("AddView") as! UINavigationController
+//        self.(vc, animated: true, completion: nil)
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        var DestViewController : AddView = segue.destinationViewController as! AddView
+//        
+//        
+//    }
+
+
+}
+
